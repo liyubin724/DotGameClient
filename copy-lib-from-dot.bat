40c8a1c91215b858a8a336c9e5559cbb1e2e44b3
li.yubin <liyubin724@163.com>
2020-07-01 23:11:25 +0800

    添加工程处理

---
+++ copy-lib-from-dot.bat
@@ -0,0 +1,26 @@
+ECHO OFF
+
+SET LibDir=..\DotGameScripts\Dot
+SET EditorTargetDir=Assets\DotEngine\Editor
+SET RuntimeTargetDir=Assets\DotEngine
+
+COPY %LibDir%\DotEngine\bin\Debug\DotEngine.dll %RuntimeTargetDir%
+COPY %LibDir%\DotEngine\bin\Debug\DotEngine.pdb %RuntimeTargetDir%
+
+COPY %LibDir%\Context\DotEngine.Context\bin\Debug\DotEngine.Context.dll %RuntimeTargetDir%
+COPY %LibDir%\Context\DotEngine.Context\bin\Debug\DotEngine.Context.pdb %RuntimeTargetDir%
+
+COPY %LibDir%\Framework\DotEngine.Framework\bin\Debug\DotEngine.Framework.dll %RuntimeTargetDir%
+COPY %LibDir%\Framework\DotEngine.Framework\bin\Debug\DotEngine.Framework.pdb %RuntimeTargetDir%
+
+COPY %LibDir%\Log\DotEngine.Log\bin\Debug\DotEngine.Log.dll %RuntimeTargetDir%
+COPY %LibDir%\Log\DotEngine.Log\bin\Debug\DotEngine.Log.pdb %RuntimeTargetDir%
+
+COPY %LibDir%\Others\PriorityQueue\bin\Debug\PriorityQueue.dll %RuntimeTargetDir%
+COPY %LibDir%\Others\PriorityQueue\bin\Debug\PriorityQueue.pdb %RuntimeTargetDir%
+
+
+COPY %LibDir%\DotEditor\bin\Debug\DotEditor.dll %EditorTargetDir%
+COPY %LibDir%\DotEditor\bin\Debug\DotEditor.pdb %EditorTargetDir%
+
+PAUSE
