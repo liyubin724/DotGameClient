65420e2300ae3f80b540a316cd92a0c8eb0fa442
li.yubin <liyubin724@163.com>
2020-07-13 15:45:42 +0800

    添加UGUI对Bitmap Font的支持

---
+++ Assets/DotEngine/UI/BitmapFontUIText.cs
@@ -0,0 +1,32 @@
+﻿using DotEngine.Fonts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DotEngine.UI
+{
+    [RequireComponent(typeof(Text))]
+    [ExecuteInEditMode]
+    public class BitmapFontUIText : BitmapFontText
+    {
+        public Text uiText = null;
+
+        protected override void OnTextChanged(string mappedText)
+        {
+            if(uiText == null)
+            {
+                uiText = GetComponent<Text>();
+            }
+
+            if(uiText!=null)
+            {
+                if(uiText.font != FontData.bmFont)
+                {
+                    uiText.font = FontData.bmFont;
+                }
+                uiText.text = mappedText;
+            }
+        }
+    }
+}
+
+
